703720c02dceef31243ea24e37d635ac07657cfc
pro972 <kerrygu8@gmail.com>
2024-11-23 19:53:26 +0800

    Changed features of the maximum review mark

--- models/campground.js
+++ models/campground.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require('./review')
 
 const CampgroundSchema = new Schema({
     title: String, image: String, price: String,
     description: String, location: String,
-    reviews: [{ type: Schema.Types.ObjectId, ref: "review" }]
+    reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }]
 });
 
 // middleware is trigger after Campground.findByIdAndDelete(id) is called, and
@@ -12,7 +13,7 @@ const CampgroundSchema = new Schema({
 CampgroundSchema.post('findOneAndDelete', async function (doc) { 
     // if campground object is non-empty
     if (doc) { 
-        await review.deleteMany({
+        await Review.deleteMany({
             _id: {
                 $in:doc.reviews
             }
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -44,12 +44,12 @@ router.get('/:id', catchAsync(async (req, res,) => {
 }));
 
 router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
+    const campground = await Campground.findById(req.params.id);
     if (!campground) {
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds');
     }
-    res.render('campgrounds/edit', { campground });
+    res.render('campgrounds/edit', { campground});
 }))
 
 router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
