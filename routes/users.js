8f289c4cf6af7b2abc844ac4524e59c36449c7cc
pro972 <kerrygu8@gmail.com>
2024-11-23 18:47:04 +0800

    modified code, bugs fixed, addded utils extension

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,29 @@
 const User = require('../models/user');
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const catchAsync = require('../utils/catchAsync')
-router.get('/register', (req, res) => {
-    res.render('users/register');
-});
+
 
 router.get("/login", (req, res) => { 
     res.render('users/login')
 })
+
+router.post("/login", passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'welcome back!');
+    res.redirect('/campgrounds');
+})
     
+router.get('/register', (req, res) => {
+    res.render('users/register');
+});
+
 router.post('/register', catchAsync(async(req, res) => { 
     try {
         const { email, username, password } = req.body
-        const user = new User({ email, username });
+        const user = new User({ email, username, password});
         // register() is from passport validation:initialize a user
-        const registeredUser = await User.register(User, Password);
+        const registeredUser = await User.register(user, password);
         req.flash('success', 'Welcome to the Camp!');
         res.redirect('/campgrounds');
     
@@ -23,6 +31,7 @@ router.post('/register', catchAsync(async(req, res) => {
     catch (e) { 
         // when the register info is not valid -> flash error message at top,user won't
         //be redirected to another page
+        console.log(e);
         req.flash('eror', e.message);
         res.redirect('/register');
     }  
