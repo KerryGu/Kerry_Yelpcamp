bec213d8abd9a4db69da4d62e88dbb45e0678ed5
pro972 <kerrygu8@gmail.com>
2024-11-24 19:07:29 +0800

    modified isLoggedin middleware for verification. Updated navbar features with Login, register, and logout options

--- middleware.js
+++ middleware.js
@@ -1,4 +1,7 @@
+
 module.exports.isLoggedIn = (req, res, next) => {
+    //req.user : passport autimatically deserialize and take out the user from the session 
+    ondeviceorientationabsolute.log("REQ.USER...", req.user);
     if (!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl
         req.flash('error', 'You must be signed in first!');
