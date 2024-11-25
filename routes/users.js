f95f620aabf42b0df3981ec4cace2cc05c6732ba
pro972 <kerrygu8@gmail.com>
2024-11-25 18:07:29 +0800

    Modified UI and added showing review or campground authors feature

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,26 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync')
+const { storeReturnTo} = require('../middleware')
 
 
 router.get("/login", (req, res) => { 
     res.render('users/login')
 })
 
-router.post("/login", passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-    req.flash('success', 'welcome back!');
-    res.redirect('/campgrounds');
+
+//By using the storeReturnTo middleware function, we can save the returnTo value to res.locals before passport.authenticate() clears the session and deletes req.session.returnTo.
+router.post("/login",
+    // use the storeReturnTo middleware to save the returnTo value from session to res.locals
+    storeReturnTo,
+    // passport.authenticate logs the user in and clears req.session
+    passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),   
+    // use res.locals.returnTo to redirect the user after login
+    (req, res) => {
+        req.flash('success', 'welcome back!');
+        const redirectUrl = res.locals.returnTo || '/campgrounds'; // store the page to return to or the campground page ( if there is no page to return to )
+        console.log(redirectUrl);
+    res.redirect(redirectUrl );
 })
     
 router.get('/register', (req, res) => {
